6f282318d7e787c1dd95ea16d3836f24addd8a8a
reducedcarpet <michaelcadenpike@gmail.com>
2024-02-06 17:35:59 +0000

    Removing Test Sub Directory

--- lib/stories.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:storybook_flutter/storybook_flutter.dart';
-
-List<Story> stories = [
-  Story(
-    name: 'TestPage',
-    builder: (_) => const Placeholder(),
-  ),
-];
